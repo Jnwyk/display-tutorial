1b75fd535b5e5e06fc5100bae4977fd5abdd5ffc
Jnwyk <jan.nowak.personal@gmail.com>
2025-08-05 20:01:24 +0200

    add selected button class and change colors

--- src/Selector.jsx
+++ src/Selector.jsx
@@ -1,17 +1,30 @@
+import { useEffect, useState } from 'react';
+
 export default function Selector({
   cssProperty,
   cssPropertyValues,
   handleOnPropertyValueChange,
 }) {
+  const [isButtonActive, setIsButtonActive] = useState(0);
+
+  const handleButtonClick = (property, buttonIndex) => {
+    handleOnPropertyValueChange(cssProperty, property);
+    setIsButtonActive(buttonIndex);
+  };
+
   return (
     <div className="property_container">
       <p>{cssProperty}</p>
-      {cssPropertyValues.map((property) => {
+      {cssPropertyValues.map((property, i) => {
         return (
           <button
-            className="selector_button"
+            className={
+              isButtonActive === i
+                ? 'selector_button_active'
+                : 'selector_button'
+            }
             key={property}
-            onClick={() => handleOnPropertyValueChange(cssProperty, property)}
+            onClick={() => handleButtonClick(property, i)}
           >
             {property}
           </button>
